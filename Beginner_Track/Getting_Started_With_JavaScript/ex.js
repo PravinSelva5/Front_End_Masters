5815140e23b3c131bf6e970cd6960e0b51ac2a51
PravinSelva5 <selvarap@mcmaster.ca>
2020-10-05 22:28:52 -0400

    updated notes

--- Beginner_Track/Getting_Started_With_JavaScript/ex.js
+++ Beginner_Track/Getting_Started_With_JavaScript/ex.js
@@ -21,3 +21,36 @@ addFavoriteBook("Great Expectations");
 addFavoriteBook("You Don't Know JS");
 
 printFavoriteBooks();
+
+
+// Falsy values -->   "", 0, -0, null , NaN, false, undefined
+// Truthy values -->  everything else other than Falsy values
+
+/*
+ 
+Coercion Best Practices
+
+Useful vs Dangerous?
+ 
+- Useful is when the reader is focused on what's important
+- Dangerous is when the reader can't tell what will happen
+
+- If the reader understands the code better than before, than THAT CODE IS BETTER.
+
+
+
+Checking Equality [ == vs ===]
+
+ == allows coercion (types different)
+ === disallows coercion (types same)
+
+ WHEN THE TYPES ARE THE SAME, THE DOUBLE EQUAL & TRIPLE EQUAL PREFORM THE SAME FUNCTION
+ 
+
+
+
+ == is about comparisons with known type(s), optionally where conversions are helpful
+
+ 
+
+*/
